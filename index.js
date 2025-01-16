61584216e1dac97ad14ef8cd4d9fa777212bf980
Nuria Mohamed Hussein <nuria.hussein@student.moringaschool.com>
2025-01-17 00:51:53 +0300

    commit all

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(names, driver) {
+  return names.filter(name => name.toUpperCase() === driver.toUpperCase());
+}
+findMatching(drivers, "Bobby");
+
+function fuzzyMatch(names, query) {
+  return names.filter(name => name.toLowerCase().startsWith(query.toLowerCase()));
+}
+
+ fuzzyMatch(drivers, 'sa');
+
+ const driversAddress = [
+  {
+    name: 'Bobby',
+    hometown: 'Pittsburgh' },
+  {
+    name: 'Sammy',
+    hometown: 'New York' } ,
+  {
+    name: 'Sally',
+    hometown: 'Cleveland' },
+  {
+    name: 'Annette',
+    hometown: 'Los Angeles' },
+  {
+    name: 'Bobby',
+    hometown: 'Tampa Bay' }
+];
+
+function matchName(drivers, name) {
+  return drivers.filter(driver => driver.name === name);
+}
+
+matchName(driversAddress, 'Bobby');
